4c8a2aa36d2ae0b51809f4d3212eb2b5caea3e43
Polarvear <znzpzp13@naver.com>
2022-10-25 23:49:34 +0900

    app.js 수정 및 파일 수정

--- spa/src/pages/Home.js
+++ spa/src/pages/Home.js
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h1> 홈 </h1>
       <p>최초 보여지는 페이지입니다.</p>
+      <ul>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles/phill">phill 소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong 소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles/void">존재하지 않는 프로필</Link>
+        </li>
+      </ul>
     </div>
   );
 };
