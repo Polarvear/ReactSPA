1d6f60221bd568a2983f1f8861a66d3cf23f9f1e
Polarvear <znzpzp13@naver.com>
2022-10-27 23:55:17 +0900

    Hooks 를 통한 쿼리스트링 파싱

--- spa/src/pages/About.js
+++ spa/src/pages/About.js
@@ -1,8 +1,20 @@
+import { useLocation, useSearchParams } from "react-router-dom";
+
 const About = () => {
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get("detail");
+  const mode = searchParams.get("mode");
+
+  const onToggleDetail = () => {
+    setSearchParams({});
+  };
+
   return (
     <div>
       <h1>소개</h1>
       <p>라우터 사용 프로젝트</p>
+      <p>쿼리스트링: {location.search}</p>
     </div>
   );
 };
