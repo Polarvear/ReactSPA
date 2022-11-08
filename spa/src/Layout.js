132c94bdd98772e751a058c2b608f787c35dcae1
Polarvear <znzpzp13@naver.com>
2022-11-08 23:54:35 +0900

    css 설정

--- spa/src/Layout.js
+++ spa/src/Layout.js
@@ -3,7 +3,14 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div>
-      <header style={{}}></header>
+      <header style={{ background: "lightgray", padding: 16, fontSize: 24 }}>
+        Header
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 };
+
+export default Layout;
